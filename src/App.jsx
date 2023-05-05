1240c5531d23ffa46ae3f1c61fcb59f51b43a110
Juan Morales <juan1810.jm@gmail.com>
2023-05-05 14:48:55 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import ClassComponents from './components/classComponents'
+import FuncComponent from './components/funcComponents'
+
+function App() {
+  return (
+    <div>
+      <ClassComponents />
+      <FuncComponent />
+    </div>
+  )
+}
+
+export default App
